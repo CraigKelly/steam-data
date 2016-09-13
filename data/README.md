e35d9a1ec54fcb723d6f23f797ece0a585453901
Craig Kelly <craig.n.kelly@gmail.com>
2016-09-13 15:26:36 -0500

    add restart logic and query saving to data/games.py

--- data/README.md
+++ data/README.md
@@ -7,9 +7,3 @@ You should have Python 3 and `make` installed to use the `Makefile`
 
 Please be careful with `make clean` - it will delete the Python virtual env
 and all previous data, so be sure that's what you want.
-
-## In Progress
-
-TODO: add app id and name from csv file to record - including for success=false
-TODO: games.py should pre-scan games.json, find missing appid's, and then append
-TODO: above todo means our makefile would be incomplete
--- data/games.py
+++ data/games.py
@@ -6,58 +6,94 @@
 
 import csv
 import json
-import requests
 import time
+import requests
+import logging
+
+
+def parse_id(i):
+    """Since we deal with both strings and ints, force appid to be correct."""
+    try:
+        return int(str(i).strip())
+    except:
+        return None
 
 
 def id_reader():
     """Read the previous created idlist.csv."""
-    print("Reading idlist.csv")
     with open("idlist.csv") as basefile:
         reader = csv.DictReader(basefile)
         for row in reader:
-            yield row['ID']
+            yield parse_id(row['ID']), row['Name']
+
+
+def previous_results():
+    """Return a set of all previous found ID's."""
+    all_ids = set()
+    with open("games.json") as f:
+        for line in f:
+            rec = json.loads(line)
+            appid = parse_id(rec.get('query_appid', None))
+            if appid:
+                all_ids.add(appid)
+    return all_ids
 
 
 def main():
     """Entry point."""
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('requests').setLevel(logging.DEBUG)
+    log = logging.getLogger('games')
+
     defaults = {
         'cc': 'US',
         'l': 'english',
         'v': '1',
     }
-    print("Default parameters: %s" % repr(defaults))
+    log.info("Default parameters: %s", repr(defaults))
 
     URL = "http://store.steampowered.com/api/appdetails/"
     LIMIT = 190
     WAIT_TIME = 5 * 60  # 5 minutes
 
-    print("Opening games.json")
+    log.info("Getting previous results from games.json")
+    previos_ids = previous_results()
 
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('requests').setLevel(logging.DEBUG)
+    log.info("Opening games.json")
+    with open("games.json", "a") as f:
+        count, batch_count = 0, 0
 
-    with open("games.json", "w") as f:
-        count = 0
-        batch_count = 0
-        for appid in id_reader():
+        log.info("Opening idlist.csv")
+        for appid, appname in id_reader():
+            if appid in previos_ids:
+                log.info("Skipping previously found id %d", appid)
+                continue
+
             req_data = dict(defaults)
             req_data['appids'] = str(appid)
 
             resp_data = requests.get(URL, params=req_data)
 
-            for appid, game in resp_data.json().items():
+            # Loop is an artifact of Steam's past suppot for multiple appid
+            # queries: we use it to insure we got back what we asked for - the
+            # details for appid
+            for result_appid, game in resp_data.json().items():
+                if parse_id(result_appid) != appid:
+                    log.error("Invalid - %s does not match requested id %d", result_appid, appid)
+
+                game['query_appid'] = appid
+                game['query_appname'] = appname
                 f.write(json.dumps(game) + '\n')
+
                 count += 1
 
             batch_count += 1
             if batch_count >= LIMIT:
-                print("batch count is %d, waiting for %d secs" % (batch_count, WAIT_TIME))
+                log.info("batch count is %d, waiting for %d secs", batch_count, WAIT_TIME)
                 time.sleep(WAIT_TIME)
                 batch_count = 0
 
-    print("Game records written: %d" % count)
+    log.info("Game records written: %d", count)
 
 
 if __name__ == "__main__":
