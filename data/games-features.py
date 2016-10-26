0bf76af9010837e243af37824db2d1a4c7a71a48
Craig Kelly <craig.n.kelly@gmail.com>
2016-10-26 16:10:49 -0500

    Add 'easy' string and numeric features to csv

--- data/games-features.py
+++ data/games-features.py
@@ -1,9 +1,13 @@
 #!/usr/env/bin python
 
+# pylama:ignore=E501
+
 """Create games-features.csv from games.json."""
 
 import csv
 import json
+import re
+import unicodedata
 
 
 def game_reader():
@@ -21,17 +25,108 @@ def game_reader():
             yield rec
 
 
-COLUMNS = ["ID", "Name"]  # TODO: make sure matches record() func below
+html_tags = re.compile('<[^<]+?>')
+
+
+def txt(t):
+    """Return filtered text."""
+    # Ascii-compatible UTF8 string
+    s = unicodedata.normalize('NFKD', str(t)).encode('ascii', 'ignore').decode('utf-8')
+    # No html tags
+    s = html_tags.sub('', s).strip()
+    # Just kill any troublesome characters
+    for c in ",'\"\r\n\t":
+        s = s.replace(c, '')
+    return s.strip()
+
+
+def num(n):
+    """Return correctly interpreted number (current assume ints)."""
+    n = str(n).strip() if n else None
+    if not n:
+        return ""
+    try:
+        return int(n)
+    except:
+        return ""
+
+
+# List of columns in the order we want them in the file
+# If you change the function `record`, you probably want to change
+# this list
+# TODO: make sure matches record() func below
+COLUMNS = [
+    "QueryID", "ResponseName", "QueryName", "ResponseName",
+
+    "RequiredAge",
+
+    # Text fields (potentially long)
+    "ResponseName",
+    "AboutText",
+    "Background",
+    "ShortDescrip",
+    "DetailedDescrip",
+    "DRMNotice",
+    "ExtUserAcctNotice",
+    "HeaderImage",
+    "LegalNotice",
+    "Reviews",
+    "SupportedLanguages",
+    "Website",
+]
 
 
 def record(raw):
     """Convert the raw JSON dict to our CSV record."""
-    data = raw["data"]  # NOQA  (TODO: actually use this)
+    data = raw["data"]
     return {
-        "ID": raw["query_appid"],
-        "Name": raw["query_appname"]
-        # TODO: all the other columns
+        "QueryID": raw["query_appid"],
+
+        # Text fields
+        "QueryName": raw["query_appname"],
+        "ResponseName": txt(data.get('name', '')),
+        "AboutText": txt(data.get('about_the_game', '')),
+        "Background": txt(data.get('background', '')),
+        "ShortDescrip": txt(data.get('short_description', '')),
+        "DetailedDescrip": txt(data.get('detailed_description', '')),
+        "DRMNotice": txt(data.get('drm_notice', '')),
+        "ExtUserAcctNotice": txt(data.get('ext_user_account_notice', '')),
+        "HeaderImage": txt(data.get('header_image', '')),
+        "LegalNotice": txt(data.get('legal_notice', '')),
+        "Reviews": txt(data.get('reviews', '')),
+        "SupportedLanguages": txt(data.get('supported_languages', '')),
+        "Website": txt(data.get('website', '')),
+
+        # Numeric fields
+        "ResponseID": num(data.get('steam_appid', None)),
+        "RequiredAge": num(data.get('required_age', None)),
     }
+
+    """ TODO
+    achievements	6615	1	Get ‘total’ as int from dict, get len(‘highlighted’) as int from dict
+    categories	11813	1	List of dict’s – boolean if descrip avail for: see NB
+    controller_support	3391	1	either ‘full’ or missing – boolean
+    demos	1134	1	List – use count (DemoCount)
+    developers	11402	1	List of strings – 3 field: MainDev, OtherDevs, DevCount = d[0], d[1:], len(d)
+    dlc	2076	1	List of ID’s – use count
+    genres	11294	1	List of dict’s – use description to discretize: see notebook
+    is_free	12037	2	boolean
+    linux_requirements	12037	2	Requirements – see nb for description
+    mac_requirements	12037	2	Requirements – see nb for description
+    metacritic	2263	1	Dict – extract key ‘score’ as int – blank on missing/failure
+    movies	10429	1	Turn list of dict’s into count
+    package_groups	12037	1	3 bool columns – see code in nb
+    packages	10193	1	PackageCount – from len of list
+    pc_requirements	12037	2	Requirements – see nb for description
+    platforms	12037	1	3 bool columns from dict: Windows, Linux, Mac – keys are (“windows”, “linux”, “mac”)
+    price_overview	10008	1	Dict – get PriceCurrency, PriceInitial, PriceFinal from keys “currency”, “initial”, “final”
+    publishers	12037	1	List – make PublishersCount, but drop blanks and uniq-ify
+    recommendations	4832	1	Use key ‘total’ to get RecommendationCount
+    release_date	12037	1	ReleaseDate using key “date”
+    screenshots	11336	1	ScreenshotCount is len of list
+    support_info	12037	1	SupportEmail and SupportURL from keys “email” and “url”
+    type	12037	1	ignore
+    """
 
 
 def main():
@@ -42,7 +137,8 @@ def main():
         writer.writerow(COLUMNS)
         count = 0
         for game in game_reader():
-            writer.writerow(record(game))
+            rec = record(game)
+            writer.writerow([rec[c] for c in COLUMNS])
             count += 1
     print("Game Records Written: %d" % count)
 
